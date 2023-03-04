2d3d10d892b8ae87484b4faf3c18639ebc9677b6
zRustyz <Russell.liu05@gmail.com>
2023-03-04 13:15:41 -0800

    B complete

--- problem-b/src/components/App.js
+++ problem-b/src/components/App.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AboutNav } from './Navigation';
+import { BreedNav } from './Navigation';
+import PetList from './PetList';
+
 
 function App(props) {
+
+  const [list, updatePets] = useState(props.pets);
+
+  let uniqueBreeds = [];
+  props.pets.forEach((pet) => {
+    if (!uniqueBreeds.includes(pet.breed)) {
+      uniqueBreeds.push(pet.breed);
+    }
+  })
+
+  function adoptPet(name) {
+    const newArr = list.map((pet) => {
+      if (pet.name === name) {
+        pet.adopted = true;
+      }
+      return pet;
+    })
+    updatePets(newArr);
+  }
+
+
   return (
-    <div>Replace this with your content!</div>
+    <div>
+    <header className="jumbotron jumbotron-fluid py-4">
+      <div className="container">
+        <h1>Adopt a Pet</h1>
+      </div>
+    </header>
+
+    <main className="container">
+      <div className="row">
+        <div id="navs" className="col-3">
+          <BreedNav breeds={uniqueBreeds} />
+          <AboutNav />
+        </div>
+
+        <div id="petList" className="col-9">
+          <PetList pets={list} adoptCallback={adoptPet}/>
+        </div>
+      </div>
+    </main>
+
+    <footer className="container">
+      <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+    </footer>
+  </div>
   );
 }
 
--- problem-b/src/components/PetList.js
+++ problem-b/src/components/PetList.js
@@ -1,1 +1,26 @@
 import React from 'react';
+
+function PetCard(pet, adoptCallback) {
+  return(
+    <div className="card" key={pet.name} onClick={() => adoptCallback(pet.name)}>
+      <img className="card-img-top" src={pet.img} alt={pet.name} />
+      <div className="card-body">
+        <h3 className="card-title">{pet.name}{pet.adopted ? ' (Adopted)' : ''}</h3>
+        <p className="card-text">{pet.sex + " " +pet.breed}</p>
+      </div>
+    </div>
+  )
+}
+
+function PetList(props) {
+  return (
+    <div>
+      <h2>Dogs for Adoption</h2>
+      <div className='card-deck'>
+      {props.pets.map((pet) => PetCard(pet, props.adoptCallback))}
+      </div>
+    </div>
+  )
+}
+
+export default PetList;
